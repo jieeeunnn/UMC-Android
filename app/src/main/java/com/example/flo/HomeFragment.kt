7d54a11437f381c503f380366d791f01cc71f53d
lje2000lje <lje2000lje@naver.com>
2023-10-04 00:35:12 +0900

    #3 HomeFragment HomePanelBanner 자동 슬라이드

--- app/src/main/java/com/example/flo/HomeFragment.kt
+++ app/src/main/java/com/example/flo/HomeFragment.kt
@@ -1,16 +1,22 @@
 package com.example.flo
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
+    private val timer = Timer()
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,16 +35,31 @@ class HomeFragment : Fragment() {
         binding.homeBannerVp.adapter = bannerAdapter// viewPager와 Adapter 연결
         binding.homeBannerVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL // ViewPager가 좌우로 스크롤 될 수 있도록 설정
 
-        val pannelAdapter = BannerVPAdapter(this)
-        pannelAdapter.addFragment(HomePannelBannerFragment(R.drawable.img_first_album_default))
-        pannelAdapter.addFragment(HomePannelBannerFragment(R.drawable.img_first_album_default))
-        pannelAdapter.addFragment(HomePannelBannerFragment(R.drawable.img_first_album_default))
-        pannelAdapter.addFragment(HomePannelBannerFragment(R.drawable.img_first_album_default))
-        binding.homePannelBackgroundVp.adapter = pannelAdapter
-        binding.homePannelBackgroundVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        val panelAdapter = BannerVPAdapter(this)
+        panelAdapter.addFragment(HomePanelBannerFragment(R.drawable.img_first_album_default))
+        panelAdapter.addFragment(HomePanelBannerFragment(R.drawable.img_first_album_default))
+        panelAdapter.addFragment(HomePanelBannerFragment(R.drawable.img_first_album_default))
+        panelAdapter.addFragment(HomePanelBannerFragment(R.drawable.img_first_album_default))
+        binding.homePanelBackgroundVp.adapter = panelAdapter
+        binding.homePanelBackgroundVp.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
-        binding.homePannelIndicator.setViewPager(binding.homePannelBackgroundVp)
+        binding.homePanelIndicator.setViewPager(binding.homePanelBackgroundVp)
+        startAutoSlide(panelAdapter)
 
         return binding.root
+    }
+
+    private fun startAutoSlide(adpater : BannerVPAdapter) {
+        // 일정 간격으로 슬라이드 변경 (3초마다)
+        timer.scheduleAtFixedRate(3000, 3000) { // 초기 지연 시간 3초 후에 작업 시작, 그 이후엔 매 3초마다 작업 반복 실행
+            handler.post {
+                val nextItem = binding.homePanelBackgroundVp.currentItem + 1 // ViewPager의 현재 페이지를 가져와 1을 더함
+                if (nextItem < adpater.itemCount) {
+                    binding.homePanelBackgroundVp.currentItem = nextItem
+                } else {
+                    binding.homePanelBackgroundVp.currentItem = 0 // 마지막 페이지에서 첫 페이지로 순환
+                }
+            }
+        }
     }
 }
