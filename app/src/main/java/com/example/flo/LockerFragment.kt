123dbac5ab80a34583628094589289ad98c572ee
lje2000lje <lje2000lje@naver.com>
2023-10-03 23:35:50 +0900

    #3 LockerFragment tabLayout, ViewPager 연결

--- app/src/main/java/com/example/flo/LockerFragment.kt
+++ app/src/main/java/com/example/flo/LockerFragment.kt
@@ -6,10 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentLockerBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LockerFragment : Fragment() {
 
     lateinit var binding: FragmentLockerBinding
+    private val locker = arrayListOf("저장한 곡", "음악파일")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,6 +19,13 @@ class LockerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLockerBinding.inflate(inflater, container, false)
+
+        val lockerAdapter = LockerVPAdapter(this)
+        binding.lockVp.adapter = lockerAdapter
+        TabLayoutMediator(binding.lockTb, binding.lockVp) { // tabLayout과 viewPager 연결
+            tab, position ->
+            tab.text = locker[position]
+        }.attach()
 
         return binding.root
     }
