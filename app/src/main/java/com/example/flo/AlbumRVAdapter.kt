b657ca14aa2783219e1483867d4dd72549c29a5e
Lee <lje2000lje@naver.com>
2023-11-07 01:33:52 +0900

    #6 앨범 RecyclerView Adapter 추가

---
+++ app/src/main/java/com/example/flo/AlbumRVAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.flo
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.flo.databinding.ItemAlbumBinding
+
+class AlbumRVAdapter(private val albumList: ArrayList<Album>): RecyclerView.Adapter<AlbumRVAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): AlbumRVAdapter.ViewHolder { // 뷰 홀더를 생성할 때 호출
+        // 아이템 뷰 객체를 만들고 이를 재활용 하기 위해 뷰 홀더에 던져줌
+        val binding: ItemAlbumBinding = ItemAlbumBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AlbumRVAdapter.ViewHolder, position: Int) { // 뷰 홀더에 데이터를 바인딩 해줘야 할 때마다 호출
+        holder.bind(albumList[position])
+    }
+
+    override fun getItemCount(): Int = albumList.size // 데이터 세트의 크기를 알려주는 함수. recyclerView의 마지막이 언제인지를 알게 해줌
+
+    inner class ViewHolder(val binding: ItemAlbumBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(album: Album) {
+            binding.itemAlbumTitleTv.text = album.title
+            binding.itemAlbumSingerTv.text = album.singer
+            binding.itemAlbumCoverImgIv.setImageResource(album.coverImg!!)
+        }
+    }
+}
