38015043999b41c311a4ed91788f608e208f5e2b
Lee <lje2000lje@naver.com>
2023-11-14 14:40:48 +0900

    #7 song data class ForeignKey 설정

--- app/src/main/java/com/example/flo/Song.kt
+++ app/src/main/java/com/example/flo/Song.kt
@@ -1,9 +1,16 @@
 package com.example.flo
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "SongTable")
+@Entity(tableName = "SongTable", foreignKeys = [
+    ForeignKey(
+    entity = Album::class,
+    parentColumns = ["id"],
+    childColumns = ["albumIdx"],
+    )]
+)
 data class Song(
     var title: String = "",
     var singer: String = "",
@@ -12,7 +19,8 @@ data class Song(
     var isPlaying: Boolean = false,// 노래가 재생 중인지
     var music: String = "",// 어떤 음악이 재생 중인지
     var coverImg: Int? = null,
-    var isLike: Boolean = false
+    var isLike: Boolean = false,
+    var albumIdx : Int = 0
 ){
     @PrimaryKey(autoGenerate = true) var id : Int = 0
 }
